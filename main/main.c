2c177ea485b3196442e8bc93670a043e15637005
dingyinghao <dingyinghao@dm-maker.com>
2024-01-26 20:00:36 +0800

    整理文件

--- main/main.c
+++ main/main.c
@@ -18,12 +18,10 @@
 #include "esp_log.h"
 #include "mdns.h"
 #include "lwip/apps/netbiosns.h"
-#include "protocol_examples_common.h"
-#if CONFIG_EXAMPLE_WEB_DEPLOY_SD
-#include "driver/sdmmc_host.h"
-#endif
 
 #define MDNS_INSTANCE "esp home web server"
+#define WEB_MOUNT_POINT "/www"
+#define MDNS_HOST_NAME "rt-ctrl"
 
 static const char *TAG = "example";
 
@@ -32,7 +30,7 @@ esp_err_t start_rest_server(const char *base_path);
 static void initialise_mdns(void)
 {
     mdns_init();
-    mdns_hostname_set(CONFIG_EXAMPLE_MDNS_HOST_NAME);
+    mdns_hostname_set(MDNS_HOST_NAME);
     mdns_instance_name_set(MDNS_INSTANCE);
 
     mdns_txt_item_t serviceTxtData[] = {
@@ -44,61 +42,13 @@ static void initialise_mdns(void)
                                      sizeof(serviceTxtData) / sizeof(serviceTxtData[0])));
 }
 
-#if CONFIG_EXAMPLE_WEB_DEPLOY_SEMIHOST
-esp_err_t init_fs(void)
-{
-    esp_err_t ret = esp_vfs_semihost_register(CONFIG_EXAMPLE_WEB_MOUNT_POINT);
-    if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to register semihost driver (%s)!", esp_err_to_name(ret));
-        return ESP_FAIL;
-    }
-    return ESP_OK;
-}
-#endif
-
-#if CONFIG_EXAMPLE_WEB_DEPLOY_SD
-esp_err_t init_fs(void)
-{
-    sdmmc_host_t host = SDMMC_HOST_DEFAULT();
-    sdmmc_slot_config_t slot_config = SDMMC_SLOT_CONFIG_DEFAULT();
-
-    gpio_set_pull_mode(15, GPIO_PULLUP_ONLY); // CMD
-    gpio_set_pull_mode(2, GPIO_PULLUP_ONLY);  // D0
-    gpio_set_pull_mode(4, GPIO_PULLUP_ONLY);  // D1
-    gpio_set_pull_mode(12, GPIO_PULLUP_ONLY); // D2
-    gpio_set_pull_mode(13, GPIO_PULLUP_ONLY); // D3
-
-    esp_vfs_fat_sdmmc_mount_config_t mount_config = {
-        .format_if_mount_failed = true,
-        .max_files = 4,
-        .allocation_unit_size = 16 * 1024
-    };
-
-    sdmmc_card_t *card;
-    esp_err_t ret = esp_vfs_fat_sdmmc_mount(CONFIG_EXAMPLE_WEB_MOUNT_POINT, &host, &slot_config, &mount_config, &card);
-    if (ret != ESP_OK) {
-        if (ret == ESP_FAIL) {
-            ESP_LOGE(TAG, "Failed to mount filesystem.");
-        } else {
-            ESP_LOGE(TAG, "Failed to initialize the card (%s)", esp_err_to_name(ret));
-        }
-        return ESP_FAIL;
-    }
-    /* print card info if mount successfully */
-    sdmmc_card_print_info(stdout, card);
-    return ESP_OK;
-}
-#endif
-
-#if CONFIG_EXAMPLE_WEB_DEPLOY_SF
 esp_err_t init_fs(void)
 {
     esp_vfs_spiffs_conf_t conf = {
-        .base_path = CONFIG_EXAMPLE_WEB_MOUNT_POINT,
+        .base_path = WEB_MOUNT_POINT,
         .partition_label = NULL,
         .max_files = 5,
-        .format_if_mount_failed = false
-    };
+        .format_if_mount_failed = false};
     esp_err_t ret = esp_vfs_spiffs_register(&conf);
 
     if (ret != ESP_OK) {
@@ -121,7 +71,6 @@ esp_err_t init_fs(void)
     }
     return ESP_OK;
 }
-#endif
 
 void app_main(void)
 {
@@ -130,9 +79,9 @@ void app_main(void)
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     initialise_mdns();
     netbiosns_init();
-    netbiosns_set_name(CONFIG_EXAMPLE_MDNS_HOST_NAME);
+    netbiosns_set_name(MDNS_HOST_NAME);
 
-    ESP_ERROR_CHECK(example_connect());
+    // ESP_ERROR_CHECK(example_connect());
     ESP_ERROR_CHECK(init_fs());
-    ESP_ERROR_CHECK(start_rest_server(CONFIG_EXAMPLE_WEB_MOUNT_POINT));
+    ESP_ERROR_CHECK(start_rest_server(WEB_MOUNT_POINT));
 }
