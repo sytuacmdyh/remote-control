7a59d269193368e5af98ccb16ebf5d885ca1e76f
dingyinghao <dingyinghao@dm-maker.com>
2024-01-28 15:58:13 +0800

    添加vuetify

--- front/remote-ctrl/src/App.vue
+++ front/remote-ctrl/src/App.vue
@@ -15,6 +15,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
+    <!-- <v-card text="..."></v-card> -->
   </header>
 
   <RouterView />
--- front/remote-ctrl/src/main.js
+++ front/remote-ctrl/src/main.js
@@ -3,6 +3,10 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +14,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(createVuetify())
 
 app.mount('#app')
